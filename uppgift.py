605538c4a2a252d93a8033060a7b5d7be2dbce31
t0tte <61695961+t0tte@users.noreply.github.com>
2025-04-09 13:57:05 +0200

    Add files via upload
    python script that asks for the users name and age, and tells them how old they will be in 100 years. Also wishes the user a nice day.

---
+++ uppgift.py
@@ -0,0 +1,17 @@
+#Pseudokod
+#Start:
+#fråga användarens <name> 
+#hälsa <name>, fråga om <age>
+#<beräkning vilket år <name> fyller 100 år>
+#skriv ut resultatet: <year>
+#Slut
+
+
+
+name = input("Vad heter du? ")
+age = int(input(f"Hej {name}, Hur gammal är du? "))
+year = 2025 - age + 100
+#line break or \n cause it looks and makes me feel cooler :D
+print(f"Du kommer att fylla 100 år, {year}. \nHa en bra dag {name}!")
+
+
