7f29470da33a20969744c2cfe05f4ba815affb22
t0tte <61695961+t0tte@users.noreply.github.com>
2025-04-09 14:05:20 +0200

    simple python script cleanup
    cleaned up the code

--- uppgift.py
+++ uppgift.py
@@ -6,12 +6,8 @@
 #skriv ut resultatet: <year>
 #Slut
 
-
-
 name = input("Vad heter du? ")
 age = int(input(f"Hej {name}, Hur gammal är du? "))
 year = 2025 - age + 100
 #line break or \n cause it looks and makes me feel cooler :D
 print(f"Du kommer att fylla 100 år, {year}. \nHa en bra dag {name}!")
-
-
